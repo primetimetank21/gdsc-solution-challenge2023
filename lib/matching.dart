cf020e93d6ee271881e4390231d2a92b12239a1a
Angie <angie@angies-air.myfiosgateway.com>
2023-04-04 21:37:28 -0400

    Adding the import dart files

--- lib/matching.dart
+++ lib/matching.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:finlitt_gdsc/main.dart';
+import 'package:finlitt_gdsc/videoPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(MyApp());
